f6b5268d51a43d6dc51f30dbacb169371a8f5d75
francisco-iriarte <iriartef@gmail.com>
2022-07-24 21:15:35 -0300

    modificaciones de nombres de variables
    Se modificaron algunos nombres de variables y valores

--- ejemplos_clase/ejemplo_1.py
+++ ejemplos_clase/ejemplo_1.py
@@ -1,36 +1,38 @@
 # Tipos de variables [Python]
 # Ejemplos de clase
 
-# Autor: Inove Coding School
-# Version: 2.0
+# Autor: Francisco Ramòn Iriarte
+# Version: 0.1
 
-# Ejemplos varialbles numéricas (números enteros --> int)
-numero_1 = 2
+# Ejemplos variables numéricas (números enteros --> int)
+numero_1 =17
 numero_2 = 4
 
 suma = numero_1 + numero_2   # Operamos la suma
+# Cargamos en la variable suma el resultado de la adición
 # Imprimimos en consola el resultado
-print('El resultado de la suma es', suma)
+print('El resultado de la suma de ', numero_1, ' y' ,numero_2, 'es', suma)
 
-resta = numero_1 - numero_2  # Operamos la resta
-
-# Imprimimos en consola el resultado con dos métodos distintos
-print('El resultado de restar', numero_1, 'y', numero_2, 'es', resta)
+diferencia = numero_1 - numero_2  # Operamos la resta
+# Cargamos en la variable diferencia el resultado de la sustracción
+# Imprimimos en consola el resultado c
+print('El diferencia de restar', numero_1, 'y', numero_2, 'es', diferencia)
 
 # ¿Qué sucede si una variable no fue definida e intento imprimir su valor?
 # print('El valor de numero es',numero)
 
 # Ahora realizaremos ejemplos con números decimales --> float
-numero_3 = 3.0
-numero_4 = 4.5
+numero_3 = 31.0
+numero_4 = 48.5
 
 suma = numero_3 + numero_4  # Operamos la suma
 # Imprimimos en consola el resultado
-print('El resultado de la suma es', suma)
+print('El resultado de la suma(float) es', suma)
 
 resta = numero_4 - numero_3  # Operamos la resta
 
-# Imprimimos en consola el resultado con dos métodos distintos
+# Cargamos en la variable diferencia el resultado de la sustracción
+# Imprimimos en consola el resultado 
 print('El resultado de restar', numero_4, 'y', numero_3, 'es', resta)
 
 # Ahora realizaremos un ejemplo numérico mixto
@@ -39,10 +41,10 @@ suma = numero_3 + numero_1   # Operamos la suma
 # Imprimimos en consola el resultado
 print('El resultado sumar', numero_3, 'y', numero_1, 'es', suma)
 
-resta = numero_3 - numero_1  # Operamos la resta
-
-# Imprimimos en consola el resultado con dos métodos distintos
-print('El resultado de restar', numero_3, 'y', numero_1, 'es', resta)
+diferencia = numero_3 - numero_1  # Operamos la direncia
+# Cargamos en la variable diferencia el resultado de la sustracción
+# Imprimimos en consola el resultado
+print('El resultado de restar', numero_3, 'y', numero_1, 'es', diferencia)
 
 # Ensayo de división de números enteros
 division = numero_1 / numero_2
